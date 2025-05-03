9662f2b5f3aefafed42c05479eb8b556fbd451ea
F000NK <F000NK@KOMPUTER>
2025-05-03 20:18:59 +0600

    fixes

--- CiCd/Db/Add-Migrations.ps1
+++ CiCd/Db/Add-Migrations.ps1
@@ -7,14 +7,14 @@ Write-Host "Adding migration '$MigrationName'..."
 
 # BankingDataContext
 dotnet ef migrations add $MigrationName `
-  --project "../../CDR-Bank.DataAccess/CDR-Bank.DataAccess.csproj" `
-  --startup-project "../../CDR-Bank.Banking/CDR-Bank.Banking.csproj" `
+  --project "CDR-Bank.DataAccess/CDR-Bank.DataAccess.csproj" `
+  --startup-project "CDR-Bank.Banking/CDR-Bank.Banking.csproj" `
   --context BankingDataContext
 
 # IdentityDataContext
 dotnet ef migrations add $MigrationName `
-  --project "../../CDR-Bank.DataAccess/CDR-Bank.DataAccess.csproj" `
-  --startup-project "../../CDR-Bank.IdentityServer/CDR-Bank.IdentityServer.csproj" `
+  --project "CDR-Bank.DataAccess/CDR-Bank.DataAccess.csproj" `
+  --startup-project "CDR-Bank.IdentityServer/CDR-Bank.IdentityServer.csproj" `
   --context IdentityDataContext
 
 Write-Host "Migrations '$MigrationName' added successfully."
