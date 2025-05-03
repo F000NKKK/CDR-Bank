6eecc83e2c538e9e4a55786778ae0ffd1b06027a
F000NK <F000NK@KOMPUTER>
2025-05-03 16:28:12 +0600

    fix ci

--- CDR-Bank.Banking/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-# replace params
-envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < ./appsettings.json | sponge ./appsettings.json
-
-# start app
-dotnet ./VP.WTrack.ApiGateway.Server.dll --environment=$ASPNETCORE_ENVIRONMENT
