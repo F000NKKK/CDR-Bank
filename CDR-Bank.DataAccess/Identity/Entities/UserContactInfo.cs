4d8199b8071157c69ae8a48d0d9696ce7c2b8e62
F000NK <F000NK@KOMPUTER>
2025-05-03 22:20:49 +0600

    fix

--- CDR-Bank.DataAccess/Identity/Entities/UserContactInfo.cs
+++ CDR-Bank.DataAccess/Identity/Entities/UserContactInfo.cs
@@ -5,7 +5,6 @@
         public Guid Id { get; set; } = Guid.NewGuid();
         public Guid UserId { get; set; }
         public User User { get; set; } = default!;
-
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? MiddleName { get; set; }
