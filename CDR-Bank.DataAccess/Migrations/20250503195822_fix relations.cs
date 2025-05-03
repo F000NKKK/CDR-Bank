db4085b2370812281b572fdc5cf1298d382fce69
F000NK <F000NK@KOMPUTER>
2025-05-04 01:58:34 +0600

    update migr

---
+++ CDR-Bank.DataAccess/Migrations/20250503195822_fix relations.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CDR_Bank.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixrelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BankAccounts_TelephoneNumber",
+                table: "BankAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "TelephoneNumber",
+                table: "BankAccounts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TelephoneNumber",
+                table: "BankAccounts",
+                type: "varchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccounts_TelephoneNumber",
+                table: "BankAccounts",
+                column: "TelephoneNumber");
+        }
+    }
+}
---
+++ CDR-Bank.DataAccess/Migrations/IdentityData/20250503195826_fix relations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CDR_Bank.DataAccess.Migrations.IdentityData
+{
+    /// <inheritdoc />
+    public partial class fixrelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
