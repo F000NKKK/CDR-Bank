af8d2845db74dafe32576a91dfae7a643c0e377f
F000NK <F000NK@KOMPUTER>
2025-05-03 12:03:31 +0600

    added new controller

---
+++ CDR-Bank.Hub.Services/Abstractions/IAccountService.cs
@@ -0,0 +1,6 @@
+﻿namespace CDR_Bank.Hub.Services.Abstractions
+{
+    public interface IAccountService
+    {
+    }
+}
---
+++ CDR-Bank.Hub.Services/AccountService.cs
@@ -0,0 +1,8 @@
+﻿using CDR_Bank.Hub.Services.Abstractions;
+
+namespace CDR_Bank.Hub.Services
+{
+    internal class AccountService : IAccountService
+    {
+    }
+}
