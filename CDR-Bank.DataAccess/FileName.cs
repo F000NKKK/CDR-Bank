be64d6800e25f9fb5645793558041c0bdfbc45db
F000NK <F000NK@KOMPUTER>
2025-05-03 12:33:46 +0600

    added example

---
+++ CDR-Bank.DataAccess/FileName.cs
@@ -0,0 +1,35 @@
+﻿using CDR_Bank.DataAccess.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CDR_Bank.DataAccess
+{
+    internal class FileName
+    {
+        public void Test()
+        {
+            var options = new DbContextOptionsBuilder<IdentityDataContext>()
+                .UseMySql(
+                    "server=localhost;database=cd_bank;user=root;password=your_password;",
+                    new MySqlServerVersion(new Version(8, 0, 36)) // <-- Укажи версию своей MySQL
+                )
+                .Options;
+
+            using var context = new IdentityDataContext(options);
+
+            // Пример: создать пользователя
+            var user = new Identity.Entities.User
+            {
+                Id = Guid.NewGuid(),
+                Email = "test@example.com",
+                PasswordHash = "hashed_password"
+            };
+
+            context.Users.Add(user);
+            context.SaveChanges();
+
+            // Проверка: найти пользователя по email
+            var loadedUser = context.Users.FirstOrDefault(u => u.Email == "test@example.com");
+            Console.WriteLine(loadedUser?.Email);
+        }
+    }
+}
