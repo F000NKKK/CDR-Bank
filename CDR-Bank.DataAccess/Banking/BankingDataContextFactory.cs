c8ee7b588d61fd4bbeead30a6b80f41dac6d1526
F000NK <F000NK@KOMPUTER>
2025-05-03 20:47:56 +0600

    update

---
+++ CDR-Bank.DataAccess/Banking/BankingDataContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace CDR_Bank.DataAccess.Banking
+{
+    public class BankingDataContextFactory : IDesignTimeDbContextFactory<BankingDataContext>
+    {
+        public BankingDataContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<BankingDataContext>();
+
+            optionsBuilder.UseMySQL(configuration.GetConnectionString("BankingDb"));
+
+            return new BankingDataContext(optionsBuilder.Options);
+        }
+    }
+}
