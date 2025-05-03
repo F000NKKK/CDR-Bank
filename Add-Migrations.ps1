8e0967d4786b69d6b20368e15c813c072b5a5845
F000NK <F000NK@KOMPUTER>
2025-05-03 20:31:32 +0600

    fix

--- Add-Migrations.ps1
+++ Add-Migrations.ps1
@@ -11,22 +11,8 @@ dotnet ef migrations add $MigrationName `
   --startup-project "CDR-Bank.Banking/CDR-Bank.Banking.csproj" `
   --context BankingDataContext
 
-# Применение миграции для BankingDataContext
-Write-Host "Applying migration '$MigrationName' to BankingDataContext..."
-dotnet ef database update `
-  --project "CDR-Bank.DataAccess/CDR-Bank.DataAccess.csproj" `
-  --startup-project "CDR-Bank.Banking/CDR-Bank.Banking.csproj" `
-  --context BankingDataContext
-
 # IdentityDataContext - Добавление миграции
 dotnet ef migrations add $MigrationName `
-  --project "CDR-Bank.DataAccess/CDR-Bank.DataAccess.csproj" `
-  --startup-project "CDR-Bank.IdentityServer/CDR-Bank.IdentityServer.csproj" `
-  --context IdentityDataContext
-
-# Применение миграции для IdentityDataContext
-Write-Host "Applying migration '$MigrationName' to IdentityDataContext..."
-dotnet ef database update `
   --project "CDR-Bank.DataAccess/CDR-Bank.DataAccess.csproj" `
   --startup-project "CDR-Bank.IdentityServer/CDR-Bank.IdentityServer.csproj" `
   --context IdentityDataContext
